2b06c28dc0b91651b7e99fa88b317f6e2a72d963
Dominik Salchner <dom.salchner@gmail.com>
2023-05-20 12:23:49 +0200

    new: ensuring country code is capitalized

--- src/1 - ProductService/MicroserviceShopping.ProductService.Domain/Manufacturers/City.cs
+++ src/1 - ProductService/MicroserviceShopping.ProductService.Domain/Manufacturers/City.cs
@@ -11,7 +11,7 @@ namespace MicroserviceShopping.ProductService.Domain.Manufacturers
       public City(string countryCode, string zipCode, string name)
       {
          CountryCode = (countryCode is not null && countryCode.Length == 2) 
-            ? countryCode 
+            ? countryCode.ToUpper()
             : throw new ArgumentException($"{nameof(countryCode)} must conform to ISO 3166-1 Alpha-2 codes.");
          ZipCode =  Guard.Against.NullOrWhiteSpace(zipCode);
          Name = Guard.Against.NullOrWhiteSpace(name);

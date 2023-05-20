3d001a237d474faf3a3d0b7ce95eb55a6c38f686
Dominik Salchner <dom.salchner@gmail.com>
2023-05-20 21:02:51 +0200

    new: implemented basic in-memory repository for products

--- src/1 - ProductService/MicroserviceShopping.ProductService.Domain/Products/IProductRepository.cs
+++ src/1 - ProductService/MicroserviceShopping.ProductService.Domain/Products/IProductRepository.cs
@@ -4,9 +4,9 @@
    {
       Task<int> AddAsync(Product product, CancellationToken cancellationToken = default);
       Task UpdateAsync(Product product, CancellationToken cancellationToken = default);
-      Task DeleteAsync(Product product, CancellationToken cancellationToken = default);
+      Task DeleteAsync(int id, CancellationToken cancellationToken = default);
       Task<IReadOnlyList<Product>> GetByIdsAsync(IReadOnlyCollection<int> ids, CancellationToken cancellationToken = default);
-      Task<Product?> GetByIdAsync(IReadOnlyCollection<int> id, CancellationToken cancellationToken = default);
+      Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
       // NOTE: in a real world app, paging would be necessary
       Task<IReadOnlyList<Product>> SearchAsync(string searchTerm, CancellationToken cancellationToken = default);
    }
--- src/1 - ProductService/MicroserviceShopping.ProductService.Infrastructure/Repositories/Products/InMemoryProductRepository.cs
+++ src/1 - ProductService/MicroserviceShopping.ProductService.Infrastructure/Repositories/Products/InMemoryProductRepository.cs
@@ -4,34 +4,51 @@ namespace MicroserviceShopping.ProductService.Infrastructure.Repositories.Produc
 {
    internal class InMemoryProductRepository : IProductRepository
    {
+      private static readonly List<Product> products = new();
+
       public Task<int> AddAsync(Product product, CancellationToken cancellationToken = default)
       {
-         throw new NotImplementedException();
+         var newId = products.Select(m => m.Id).DefaultIfEmpty(0).Max() + 1;
+         var added = new Product(newId, product.Name, product.Description, product.Images, product.ManufacturerId);
+
+         products.Add(added);
+
+         return Task.FromResult(newId);
       }
 
-      public Task DeleteAsync(Product product, CancellationToken cancellationToken = default)
+      public async Task DeleteAsync(int id, CancellationToken cancellationToken = default)
       {
-         throw new NotImplementedException();
+         var found = await GetByIdAsync(id, cancellationToken);
+         if (found is not null)
+            products.Remove(found);
       }
 
-      public Task<Product?> GetByIdAsync(IReadOnlyCollection<int> id, CancellationToken cancellationToken = default)
+      public async Task<Product?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
       {
-         throw new NotImplementedException();
+         var products = await GetByIdsAsync(new[] { id }, cancellationToken);
+         return products.FirstOrDefault();
       }
 
       public Task<IReadOnlyList<Product>> GetByIdsAsync(IReadOnlyCollection<int> ids, CancellationToken cancellationToken = default)
       {
-         throw new NotImplementedException();
+         var found = (IReadOnlyList<Product>)products.Where(p => ids.Contains(p.Id)).ToList();
+         return Task.FromResult(found ?? new List<Product>());
       }
 
       public Task<IReadOnlyList<Product>> SearchAsync(string searchTerm, CancellationToken cancellationToken = default)
       {
-         throw new NotImplementedException();
+         var found = (IReadOnlyList<Product>)products.Where(p => p.Name.Contains(searchTerm) || p.Description.Contains(searchTerm)).ToList();
+         return Task.FromResult(found ?? new List<Product>());
       }
 
       public Task UpdateAsync(Product product, CancellationToken cancellationToken = default)
       {
-         throw new NotImplementedException();
+         var idx = products.FindIndex(p => p.Id == product.Id);
+         if (idx == -1) return Task.CompletedTask;
+
+         products[idx] = product;
+
+         return Task.CompletedTask;
       }
    }
 }
